855c0d27dedfa8c13b18d457a0e44261dadb26dd
jideabdqudus <jideabdqudus@gmail.com>
2020-03-19 17:48:21 +0100

    Added Changes

---
+++ src/previousComponents/layout/AddBtn.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const AddBtn = () => {
+  return (
+    <div className="fixed-action-btn">
+      <a
+        href="#add-post-modal"
+        className="btn-floating btn-large waves-effect waves-light red"
+      >
+        <i className="material-icons">mode_edit</i>
+      </a>
+      <ul>
+        <li>
+          <a
+            hrerf="#user-list-modal"
+            className="btn-floating green modal-trigger"
+          >
+            <i className="material-icons">person</i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default AddBtn;
